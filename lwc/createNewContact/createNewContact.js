f37e8643a67ec764b8b993153752ca89c9766706
Sparsh Khandelwal <sparsh.khandelwal@metacube.com>
2022-09-27 14:51:11 +0530

    minor changes

--- lwc/createNewContact/createNewContact.js
+++ lwc/createNewContact/createNewContact.js
@@ -1,4 +1,4 @@
-import { LightningElement, wire, api, track } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
